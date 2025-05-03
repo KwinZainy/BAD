49bc48296ee50afb158f5d50c98662a86eae24c6
Glitch4real <144991787+Glitch4real@users.noreply.github.com>
2025-05-04 00:03:44 +0100

    did more on app.js

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ const bagBtn = document.getElementById("bagBtn")
 const foodBtn = document.getElementById("foodBtn")
 const phoneBtn = document.getElementById("phoneBtn")
 const cartNumber = document.getElementById("cartNumber")
+const cartDisplay = document.getElementById("cartDisplay")
 
 
-
-function addToCart(){
+function addToCart(value){
     let counter = cartNumber.innerText
     counter++
     console.log(counter);
     
     cartNumber.innerText = counter
+    cartDisplay.innerHTML += `<p>${value}</p>`
 }
 
 
 
-bagBtn.addEventListener("click", addToCart)
-phoneBtn.addEventListener("click", addToCart)
-foodBtn.addEventListener("click", addToCart)
+// bagBtn.addEventListener("click", addToCart)
+// phoneBtn.addEventListener("click", addToCart)
+// foodBtn.addEventListener("click", addToCart)
