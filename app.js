171bd91bfcd5237e58effe77f689a8f50e42ce8f
yungflashh <streaktv8@gmail.com>
2025-05-03 23:32:12 +0100

    fixing js

--- app.js
+++ app.js
@@ -2,3 +2,19 @@ const bagBtn = document.getElementById("bagBtn")
 const foodBtn = document.getElementById("foodBtn")
 const phoneBtn = document.getElementById("phoneBtn")
 const cartNumber = document.getElementById("cartNumber")
+
+
+
+function addToCart(){
+    let counter = cartNumber.innerText
+    counter++
+    console.log(counter);
+    
+    cartNumber.innerText = counter
+}
+
+
+
+bagBtn.addEventListener("click", addToCart)
+phoneBtn.addEventListener("click", addToCart)
+foodBtn.addEventListener("click", addToCart)
