b6ba2497cde431b9ea17b28ad8a5dc90ed1d925d
yungflashh <streaktv8@gmail.com>
2025-05-03 23:17:15 +0100

    fixing js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 const bagBtn = document.getElementById("bagBtn")
 const foodBtn = document.getElementById("foodBtn")
 const phoneBtn = document.getElementById("phoneBtn")
+const cartNumber = document.getElementById("cartNumber")
