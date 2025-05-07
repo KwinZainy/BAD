21e60d5f239f3b2ae98f2daf22163c726f9a1202
untitledDev9 <ahmedsuleiman940@gmail.com>
2025-05-07 15:27:04 +0100

    we made it display on a new cart

--- app.js
+++ app.js
@@ -1,67 +1,21 @@
 
 const cartNumber = document.getElementById("cartNumber")
-const display = document.getElementById("cartItems")
 
 
 let cartArray = [];
 
 
-let words = ["This", "is", "just", "the", "beginning", "of", "learning" ,"js"]
 
-
-let lastElement = words.indexOf("of");
-
-
-let new_words = "Blessing is a chicken"
-
-// console.log(new_words.split(" "));
-
-let splitted_words = new_words.split(" ")
-
-
-
-let name = splitted_words[3]
-
-console.log(name);
-
- 
-
-
-
-console.log(lastElement);
-console.log(words.splice(lastElement,1,"REACT"))
-console.log(words);
-
-
-
-
-
-function addToCart(value){
+function addToCart(nameOfItem){
     let counter = cartNumber.innerText
     counter++
-    console.log(counter);
+    
     cartNumber.innerText = counter
-    cartArray.push(value)
+
+    cartArray.push(nameOfItem)
     
-    
-    display.innerText = ""
-    cartArray.forEach((element)=>{
-            
-            display.innerHTML += `<p> ${element} </p>`
-            // console.log(element)
-    })
 
+    localStorage.setItem("nameOfCartItems", cartArray)
 
-
-    // console.log(cartArray);
     
 }
-
-
-
-function displayCart(){
-    display.classList.toggle("cart-display")
-}
-
-
-
---
+++ cart.js
@@ -0,0 +1,31 @@
+let cartItemList = localStorage.getItem("nameOfCartItems");
+const display = document.getElementById("cartItems")
+
+
+
+console.log(cartItemList);
+
+
+function displayItems(){
+  display.innerText = ""
+
+    let arrayCarts =  cartItemList.split(",")
+
+    for (let index = 0; index < arrayCarts.length; index++) {
+          
+          let eachItem = arrayCarts[index];
+
+          display.innerHTML += `<p> ${eachItem}  </p>`  
+          
+      
+    }
+
+
+    // arrayCarts.forEach((element)=>{
+    //   display.innerHTML += `<p> ${element} </p>`
+    // })
+ 
+}
+
+
+displayItems();
